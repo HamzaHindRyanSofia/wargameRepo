9eb105ff6cf7ce691128f63457bcf2666f8808db
HamzaHind <hamza.laamouri@gmail.com>
2017-08-17 16:49:38 +0200

    First Push

---
+++ PythonAppDevSamples/OrcsV1/orcsV0_0_1.py
@@ -0,0 +1,17 @@
+'''
+Created on 16 ago 2017
+
+@author: Hamza
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
